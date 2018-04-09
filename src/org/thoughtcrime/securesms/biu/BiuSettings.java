b53912f96a37b4b03fb895fa42a9d67245959446
hemi <Leibo.Hemi@gmail.com>
2018-04-09 15:55:52 +0300

    BiuSettings logic update

--- src/org/thoughtcrime/securesms/biu/BiuSettings.java
+++ src/org/thoughtcrime/securesms/biu/BiuSettings.java
@@ -4,18 +4,31 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.util.JsonUtils;
+
+import java.io.IOException;
 
 public class BiuSettings {
 
-    public static String get(Context context, String key) {
+    enum AppMode {Original, Anonymous}
+
+    public static <T> T get(Context context, String key, Class<T> obj, T defaultValue) throws IOException {
         SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        return sharedPref.getString(key, "");
+        String v = sharedPref.getString(key, "");
+        if (v == "")
+            return defaultValue;
+
+        return JsonUtils.fromJson(v, obj);
     }
 
-    public static void set(Context context, String key, String value){
+    public static void set(Context context, String key, Object value) throws IOException {
         SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(key, value);
+        editor.putString(key, JsonUtils.toJson(value));
         editor.commit();
+    }
+
+    public static AppMode getMode(Context context) throws IOException {
+        return get(context, "mode", AppMode.class, AppMode.Original);
     }
 }
