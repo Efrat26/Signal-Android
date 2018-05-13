c16271d8e2d305a24c762d42bf254cccc22a42f1
efiso <efisofer26@gmail.com>
2018-05-13 15:59:40 +0300

    added the experiment manager activity (hidden contact)

---
+++ src/org/thoughtcrime/securesms/biu/HiddenContact/ExperimentManager.java
@@ -0,0 +1,14 @@
+package org.thoughtcrime.securesms.biu.HiddenContact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.thoughtcrime.securesms.R;
+
+public class ExperimentManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_experiment_manager);
+    }
+}
